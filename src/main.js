c53fc2071295d446d1b55dd4816493d6cf16005b
cailudys <cailudys@gmail.com>
2023-05-09 14:20:57 +0800

    安装koa相关依赖，并初步搭建一个服务器

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log("项目运行");
+const Koa = require("koa");
+
+const app = new Koa();
+
+app.use((ctx, next) => {
+  ctx.body = "服务器访问成功";
+});
+
+app.listen(8000, () => {
+  console.log("coderhub 服务器启动成功 ~");
+});

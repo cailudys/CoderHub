aafdabcbff8913d999fd84475eed7ad87fd5fa4c
cailudys <cailudys@gmail.com>
2023-05-09 14:25:34 +0800

    项目中使用koa路由

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 const Koa = require("koa");
+const koaRouter = require("@koa/router");
 
 const app = new Koa();
 
-app.use((ctx, next) => {
-  ctx.body = "服务器访问成功";
+const userRouter = new koaRouter({ prefix: "/users" });
+userRouter.get("/list", (ctx, next) => {
+  ctx.body = "users list";
 });
+
+app.use(userRouter.routes());
+app.use(userRouter.allowedMethods());
 
 app.listen(8000, () => {
   console.log("coderhub 服务器启动成功 ~");
